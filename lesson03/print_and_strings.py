0b42902e220ecaaf6aeb4532447c75cf767adc76
Boris Forinko <b317.forinko@gmail.com>
2021-02-27 11:31:07 +0300

    lesson completed

--- lesson03/print_and_strings.py
+++ lesson03/print_and_strings.py
@@ -6,3 +6,23 @@ print("x1 = " + x1)
 print("x2 = " + x2)
 
 # better variants
+
+x1 = 5.33123213
+x2 = 5.1
+
+# print with args
+print("x1 =", x1)
+print("x2 =", x2)
+
+# string formating
+# C-style
+print("x1 = %.2f" % x1)
+print("x2 = %.2f" % x2)
+
+# .format()
+print("x1 = {0}".format(x1))
+print("x2 = {root2}".format(root2=x2))
+
+# f-string
+print(f"x1 = {x1:.2f}")
+print(f"x2 = {x2}")
--- lesson03/vars.py
+++ lesson03/vars.py
@@ -7,3 +7,10 @@ hypotenuse = (float(sys.argv[1])**2+float(sys.argv[2])**2)**(1/2)
 print("Length of hypotenuse is", hypotenuse)
 
 # corrected
+
+a = float(sys.argv[1])
+b = float(sys.argv[2])
+
+hypotenuse = (a**2 + b**2)**(1/2)
+
+print(hypotenuse)
