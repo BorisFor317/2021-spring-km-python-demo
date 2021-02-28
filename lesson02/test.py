2e80ce667db46e3a4db76088d7d533099a645516
Boris Forinko <b317.forinko@gmail.com>
2021-02-28 22:08:36 +0300

    add test for demonstartion

---
+++ lesson02/test.py
@@ -0,0 +1,24 @@
+import math
+
+from sqrt import square_root
+
+
+test_data = [
+    4,
+    16,
+    2**32,
+    2**64,
+    111325,
+    111111111111111111111111111111111111111111111111111111111111111111111111
+]
+
+for x in test_data:
+    my_sqrt = square_root(x)
+    real_sqrt = math.sqrt(x)
+
+    err = abs(my_sqrt - real_sqrt)
+
+    print(f"for {x} sqrt is equal to {real_sqrt:4f}.")
+    print(f"my method result {my_sqrt}")
+    print(f"abs error of my method is {err}.")
+    print(f"rel error of my method is {err / real_sqrt}.\n")
